e743a3c7790e6c32d653a3376baa1fc65575a735
Rishikesh <rishikesh.mmf@gmail.com>
2023-11-23 13:10:58 +0530

    changes test

--- Pythoncrashcourse/pygame/game_functions.py
+++ Pythoncrashcourse/pygame/game_functions.py
@@ -1,5 +1,7 @@
 import sys
-
 import pygame
+
 def check_events():
-"""Respond to keypresses and mouse events."""
+ for event in pygame.event.get():  """Respond to keypresses and mouse events."""
+ if event.type == pygame.QUIT:
+   sys.exit()
